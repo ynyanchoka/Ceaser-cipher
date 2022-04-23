6796560c34fd494842e761825a8c9ff831a827d0
ynyanchoka <monaryymelda@gmail.com>
2022-04-24 00:44:32 +0300

    add fourth test

--- src/test/java/models/CipherTest.java
+++ src/test/java/models/CipherTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CipherTest {
-
+//encrypt test
     @Test
     public void shiftSingleCharacter_shiftsSingleCharacterAsRequiredByUser_3() {
         Encoding encoding  = new Encoding ();
@@ -15,9 +15,15 @@ public class CipherTest {
         Encoding encoding  = new Encoding ();
         assertEquals("EBIIL",encoding.encrypt("HELLO",3));
     }
-
+//decrypt test
     @Test
     public void resetsCharacter_resetsCharacterAsRequiredByUser_3() {
+        Decoding decoding  = new Decoding ();
+        assertEquals("X",decoding.decrypt("U",3));
+    }
+
+    @Test
+    public void resetsWord_resetsWordAsRequiredByUser_3() {
         Decoding decoding  = new Decoding ();
         assertEquals("THE",decoding.decrypt("QEB",3));
     }
